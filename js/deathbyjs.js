c6a189e7e8c9007f2b793b76e10f367c59f22ce3
d-herbert <deidre.m.herbert@gmail.com>
2022-05-01 14:04:00 -0700

    part 4 complete

--- js/deathbyjs.js
+++ js/deathbyjs.js
@@ -11,21 +11,39 @@ function countVowels(text) {
     let count = 0
     for (var i=0; i<=text.length - 1; i++) {
         if ('aeiou'.includes(text[i].toLowerCase())) {
-            count += 1;
+            count += 1
         }
     }
     console.log(`there are ${count} vowels in the string`)
 }
 //STEP 4
-
+function generateString(length) {
+    var text = ""
+    var charset = "abcdefghijklmnopqrstuvwxyz0123456789"
+    for (var i = 0; i < length; i++) {
+      text += charset.charAt(Math.floor(Math.random() * charset.length))
+    }
+    console.log(text)
+}
 //STEP 5
+function getLongest(text) {
+    arr = text.split(',')
+    let word = ""
+    for (let i = 0; i < arr.length; i++) {
+      if (word.length < arr[i].length) {
+        word = arr[i]
+      }
+    }
+    console.log(`the longest word is ${word}`)
+}
 
-
+// call all functions
 function main() {
-    // sortString(text=prompt('enter a string and it will return an ordered string'))
-    // capLetters(text=prompt('enter a string and it will return capitalized'))
-    // countVowels(text=prompt('enter a string and it will return the number of vowels'))
+    sortString(text=prompt('enter a string and it will return an ordered string'))
+    capLetters(text=prompt('enter a string and it will return capitalized'))
     countVowels(text=prompt('enter a string and it will return the number of vowels'))
+    generateString(length=prompt('enter an integer to return a randomly generated string of that length'))
+    getLongest(text=prompt('enter multiple country names separated by a comma'))
 }
 
 main()
