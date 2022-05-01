4751ed75b55d57c69cb332760ca3fd044f5f9f65
d-herbert <deidre.m.herbert@gmail.com>
2022-05-01 10:34:59 -0700

    steps 4-7 complete

--- js/strings.js
+++ js/strings.js
@@ -8,14 +8,17 @@ alert(`${nam[num]}`)
 let las = prompt(`enter your last name`)
 alert(`your name is: ${nam.concat(' ', las)}`)
 //STEP 4
-let tex = 'The quick brown fox jumps over the lazy fox'
-alert(`last index of fox: ${tex.lastIndexOf('fox')}`)
+let fox1 = 'The quick brown fox jumps over the lazy dog'
+alert(`last index of fox: ${fox1.lastIndexOf('fox')}`)
 //STEP 5
-
+let fox2 = 'The quick brown fox jumps over the lazy fox'
+alert(`last index of fox: ${fox2.lastIndexOf('fox')}`)
 //STEP 6
-
+let fln = nam.concat(' ', las)
+alert(`${fox1.replace('the lazy dog', fln)}`)
 //STEP 7
-
+let wrd = prompt(`enter a word from: ${fox1}`)
+alert(`${fox1.search(wrd)}`)
 //STEP 8
 
 //STEP 9
