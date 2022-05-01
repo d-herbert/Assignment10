91968dfe84c796934f20953b954f1da99d57d57e
d-herbert <deidre.m.herbert@gmail.com>
2022-05-01 10:23:33 -0700

    steps 1-4 complete

--- js/strings.js
+++ js/strings.js
@@ -1,11 +1,15 @@
 //STEP 1
-
+let nam = prompt('enter your name')
+alert(`${nam.length}`)
 //STEP 2
-
-//STEP 3
-
+let num = prompt(`enter a number between 0 and ${nam.length - 1}`)
+alert(`${nam[num]}`)
+//STEP 3deid
+let las = prompt(`enter your last name`)
+alert(`your name is: ${nam.concat(' ', las)}`)
 //STEP 4
-
+let tex = 'The quick brown fox jumps over the lazy fox'
+alert(`last index of fox: ${tex.lastIndexOf('fox')}`)
 //STEP 5
 
 //STEP 6
